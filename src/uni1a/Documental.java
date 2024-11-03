5bab20d099ab4e2d3f7b8e436a8d37bd5fcb6438
Tylam.dev <tylam.dev@gmail.com>
2024-11-02 19:10:26 -0500

    Agerga Investigadores a modelo Documental

--- src/uni1a/Documental.java
+++ src/uni1a/Documental.java
@@ -3,9 +3,15 @@
  */
 package uni1a;
 
+import NuevasClases.Investigador;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 // Subclase Documental que extiende de ContenidoAudiovisual
 public class Documental extends ContenidoAudiovisual {
     private String tema;
+    private ArrayList<Investigador> Investigadores;
 
     public Documental(String titulo, int duracionEnMinutos, String genero, String tema) {
         super(titulo, duracionEnMinutos, genero);
@@ -19,15 +25,24 @@ public class Documental extends ContenidoAudiovisual {
     public void setTema(String tema) {
         this.tema = tema;
     }
+    public void agregarInvestigardor(Investigador inv){
+        Investigadores.add(inv);
+    }
     
     @Override
     public void mostrarDetalles() {
+        String inv = "Investigadores: ";
+        for(Investigador iv: Investigadores)
+        {
+            inv = inv + iv.getNombre();
+        }
         System.out.println("Detalles de la película:");
         System.out.println("ID: " + getId());
         System.out.println("Título: " + getTitulo());
         System.out.println("Duración en minutos: " + getDuracionEnMinutos());
         System.out.println("Género: " + getGenero());
         System.out.println("Tema: " + this.tema);
+        System.out.println(inv);
         System.out.println();
     }
 }
